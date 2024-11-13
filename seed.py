27ac2893df2cf3eee0a6aa606ed9c6d11633a936
Sean Wilkins <srw134@wilkinshouse.com>
2024-11-12 22:32:49 -0500

    Update seed.py

--- seed.py
+++ seed.py
@@ -191,11 +191,11 @@ def simulate_failed_logins():
         ip = f"172.31.130.{random.randint(1, 254)}"
         port = random.randint(1025, 65535)
         message = f"Failed password for {user} from {ip} port {port} ssh2"
-        subprocess.run(["logger", "-p", "authpriv.err", message])
+        subprocess.run(["sudo", "logger", "-p", "authpriv.err", message])
         ip = f"172.31.120.{random.randint(1, 254)}"
         port = random.randint(1025, 65535)
         message = f"Failed password for {user} from {ip} port {port} ssh2"
-        subprocess.run(["logger", "-p", "authpriv.err", message])
+        subprocess.run(["sudo", "logger", "-p", "authpriv.err", message])
 
 # Set up cron jobs for each user
 def create_cron_jobs():
